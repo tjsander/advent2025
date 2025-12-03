340880494fda1c156a38d29c41f95623a5a5ef7e
Travis Sanders <tjsander@gmail.com>
2025-12-03 14:29:51 -0800

    Recursion FTW

--- 3/elf.py
+++ 3/elf.py
@@ -9,8 +9,9 @@ def main():
     Lines = (line.rstrip() for line in input_file)
     Lines = list(line for line in Lines if line)
 
-    count_1 = 0
-    count_2 = 0
+    count_1   = 0
+    count_1_2 = 0
+    count_2   = 0
 
     for line in Lines:
         # get the largest number in the line
@@ -33,65 +34,45 @@ def main():
             right_max = 10 * max_int + right_max
 
         max_max = max(left_max, right_max)
-        print(max_max)
         count_1 += max(left_max, right_max)
 
-    print("Part 2")
+    # Verification that part 2 works correctly
 
     for line in Lines:
-        max_jolt = part_2(line)
+        max_jolt = int(part2_recursive(line, 2))
+        count_1_2 += max_jolt
+
+
+
+    for line in Lines:
+        max_jolt = int(part2_recursive(line, 12))
         count_2 += max_jolt
 
-
-    # 147952302767971 is too low??
-    # 170044670732187 also too low.........
-    # 169978016428836 also wrong
-    # 3031110778619
-    # 169059991684173 WRONG
-    # 169129168648781 WRONG
-
+    print("Part 1")
     print(count_1)
+    print("Part 2 Verification")
+    print(count_1_2)
+    print("Part 2")
     print(count_2)
 
-def part_2(input_string):
-    JOLT_SIZE = 12
-    smallest_int = (min(map(int, input_string)))
-    reduced_string = input_string
-
-    # trim the input string
-    max_int = (max(map(int, input_string)))
-
-    max_idx = input_string.find(str(max_int))
-    if (len(input_string[max_idx:]) >= JOLT_SIZE):
-        reduced_string = input_string[max_idx:]
-
-    input_string = reduced_string
-
-    while (smallest_int < max_int):
-        input_string = input_string.replace(str(smallest_int), '')
-        if (len(input_string) > JOLT_SIZE):
-            reduced_string = input_string
-            smallest_int += 1
-        else:
-            break
-
-    delete_index = reduced_string.find(str(smallest_int))
-
-    while (len(reduced_string) > JOLT_SIZE):
-        reduced_string = remove_at(delete_index, reduced_string)
-
-        #DELETE ANY NUMBERS THAT ARE LESS THAN THE NEXT NUMBER UNTIL YOU REACH SMALLEST_INT
-        # while (reduced_string[delete_index] != str(smallest_int) and
-        #        int(reduced_string[delete_index]) < int(reduced_string[delete_index +1])):
-        #     reduced_string = remove_at(delete_index, reduced_string)
-        while (len(reduced_string) > JOLT_SIZE and delete_index < JOLT_SIZE
-            and int(reduced_string[delete_index]) < int(reduced_string[delete_index +1])
-            and reduced_string[delete_index] != str(smallest_int)):
-            reduced_string = remove_at(delete_index, reduced_string)
-        delete_index = reduced_string.find(str(smallest_int))
-
-    print (reduced_string)
-    return int(reduced_string)
+def part2_recursive(input_string, JOLT_SIZE):
+    """
+    Find the largest number in the remaining string that still enough remaining digits to satisfy JOLT_SIZE
+    """
+    output_string = ""
+    finished = False
+    max_capacity = (max(map(int, input_string)))
+    if (JOLT_SIZE == 1):
+        return str(max_capacity)
+    while (max_capacity > 0):
+        max_idx = input_string.find(str(max_capacity))
+        if (max_idx != -1):
+            if (len(input_string[max_idx:]) >= JOLT_SIZE):
+                output_string += str(max_capacity)
+                output_string += str(part2_recursive(input_string[max_idx+1:], JOLT_SIZE-1))
+                return output_string
+        max_capacity -= 1
+    return -1
 
 def remove_at(i: int, s: str) -> str:
     return s[:i] + s[i+1:]
