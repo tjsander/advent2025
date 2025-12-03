5de68e721638da8914ef8a8f63b708e86c88e725
Travis Sanders <tjsander@gmail.com>
2025-12-03 00:27:05 -0800

    Not quite correct...

--- 3/elf.py
+++ 3/elf.py
@@ -2,7 +2,7 @@
 
 DAY = 3
 INPUT = str(DAY) + '/input.txt'
-# INPUT = str(DAY) + '/test_input.txt'
+INPUT = str(DAY) + '/test_input.txt'
 
 def main():
     input_file = open(INPUT, 'r')
@@ -33,12 +33,46 @@ def main():
             right_max = 10 * max_int + right_max
 
         max_max = max(left_max, right_max)
-        print(max_max)
+        # print(max_max)
         count_1 += max(left_max, right_max)
 
+
+    for line in Lines:
+        max_jolt = part_2(line)
+        count_2 += max_jolt
+
+
+    # 147952302767971 is too low??
     print(count_1)
     print(count_2)
 
+def part_2(input_string):
+    SIZE_LIMIT = 2
+    smallest_int = (min(map(int, input_string)))
+    smallest_string = input_string
+
+    while (smallest_int <= 8):
+        input_string = input_string.replace(str(smallest_int), '')
+        if (len(input_string) > SIZE_LIMIT):
+            smallest_string = input_string
+            smallest_int += 1
+        else:
+            break
+
+    delete_index = smallest_string.find(str(smallest_int))
+
+    while (len(smallest_string) > SIZE_LIMIT):
+        smallest_string = remove_at(delete_index, smallest_string)
+        if (len(smallest_string) > SIZE_LIMIT and delete_index < SIZE_LIMIT
+               and int(smallest_string[delete_index]) < int(smallest_string[delete_index +1])):
+            smallest_string = remove_at(delete_index, smallest_string)
+        delete_index = smallest_string.find(str(smallest_int))
+
+    print (smallest_string)
+    return int(smallest_string)
+
+def remove_at(i: int, s: str) -> str:
+    return s[:i] + s[i+1:]
 
 if __name__ == '__main__':
     main()
