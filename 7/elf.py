c9e21833442ed140316a5611f71c696240631903
Travis Sanders <tjsander@gmail.com>
2025-12-07 16:27:17 -0800

    Day7 pt1 fin

---
+++ 7/elf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import re
+
+DAY = 7
+
+def main(test=False):
+    input_1 = str(DAY) + '/test_input.txt'
+    if not test:
+        input_1 = str(DAY) + '/input.txt'
+    input_file = open(input_1, 'r')
+    Lines = list([line[:-1] for line in input_file])
+    count_1   = 0
+    count_2   = 0
+
+    # rows = []
+    beams = []
+    beams.append(Lines[0].find('S') )
+
+    for line in Lines:
+        if line.find('^') ==  -1:
+            continue
+        # rows.append(" ".join(line.split()).split(" "))
+        splitters = [m.start() for m in re.finditer('\^', line)]
+        new_beams = []
+        for beam in beams:
+            if beam in splitters:
+                count_1 += 1
+                # beams.remove(beam)
+                new_beams.append(beam -1)
+                new_beams.append(beam +1)
+            else:
+                new_beams.append(beam)
+        beams = list(set(new_beams))
+
+    print("Part 1")
+    print(count_1)
+
+    if test:
+        for line in Lines:
+            print (line)
+
+    print("Part 2")
+    print(count_2)
+
+if __name__ == '__main__':
+    main(test=True)
+    main()
