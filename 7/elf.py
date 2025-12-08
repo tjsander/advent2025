858929cda9be459cf11551954dda77a267244878
Travis Sanders <tjsander@gmail.com>
2025-12-07 17:01:21 -0800

    Part 2 easy solution... not at all memory-efficient

--- 7/elf.py
+++ 7/elf.py
@@ -3,14 +3,14 @@ import re
 
 DAY = 7
 
-def main(test=False):
+def main(test=False, part2=False):
     input_1 = str(DAY) + '/test_input.txt'
     if not test:
         input_1 = str(DAY) + '/input.txt'
     input_file = open(input_1, 'r')
     Lines = list([line[:-1] for line in input_file])
     count_1   = 0
-    count_2   = 0
+    count_2   = 1
 
     # rows = []
     beams = []
@@ -25,23 +25,31 @@ def main(test=False):
         for beam in beams:
             if beam in splitters:
                 count_1 += 1
-                # beams.remove(beam)
+                count_2 += 1
                 new_beams.append(beam -1)
                 new_beams.append(beam +1)
             else:
                 new_beams.append(beam)
-        beams = list(set(new_beams))
+        if not part2:
+            beams = list(set(new_beams))
+        else:
+            beams = new_beams
+    # count_2 = len(beams)
 
-    print("Part 1")
-    print(count_1)
+    if not part2:
+        print("Part 1")
+        print(count_1)
 
     if test:
         for line in Lines:
             print (line)
 
-    print("Part 2")
-    print(count_2)
+    if part2:
+        print("Part 2")
+        print(count_2)
 
 if __name__ == '__main__':
     main(test=True)
     main()
+    main(test=True, part2=True)
+    # main(part2=True)
